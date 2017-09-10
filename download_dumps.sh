fc9d14900208d471339ff05c4fc0006838f33e9d
Stuart Geiger <sgeiger@gmail.com>
2017-09-10 11:40:38 -0400

    add info on downloading from DASH, remove languages we didn't use

--- download_dumps.sh
+++ download_dumps.sh
@@ -1,6 +1,32 @@
+##############################################################
+#
+# download_dumps.sh: script for downloading MediaWiki
+# metadata dumps from the Wikimedia Foundation servers
+#
+# Note that the Wikimedia foundation only keeps
+# specific versions of dumps for about six months,
+# so the April 20th, 2017 dumps we used have now
+# been removed. This script will no longer work as-is.
+#
+# This script, which we used to download the dumps from
+# dumps.wikimedia.org or a mirror, is here for purposes
+# of computational reproducibility and replayability. You
+# can change the variables below to automatically download
+# newer dumps in various languages.
+#
+# We have also archived the April 20th, 2017 dumps we used
+# for this project at https://doi.org/10.6078/D1FD3K via
+# the University of California Library's DASH project.
+# You must click a link in a browser, enter your e-mail,
+# and a link will be sent to you so you can download the
+# older dumps.
+#
+##############################################################
+
+
 #!/bin/bash
 
-declare -a langs=("de" "en" "es" "fr" "ja" "pt" "zh" "ar" "he" "hu" "ko" "ro" "fa" "cz" "ru" "meta" "simple")
+declare -a langs=("de" "en" "es" "fr" "ja" "pt" "zh")
 declare -a dump_date="20170420"
 declare -a dump_folder="/data/wikipedia/xmldatadumps/public"
 declare -a dump_mirror="https://dumps.wikimedia.your.org"
